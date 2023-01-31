82bb046998ab95e05b7e9b3ad8276b06378cc8fd
ysydorenko <yevhen.sydorenko@symphony-solutions.com>
2023-01-31 17:58:39 +0200

    Updated obol script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-sudo apt update
-wget -O get-docker.sh https://get.docker.com
-sh get-docker.sh
-sudo usermod -aG docker $USER
-apt-get install docker-compose -y
-sudo chmod +x /usr/bin/docker-compose
-docker -v
-docker-compose --version
-rm -rf get-docker.sh
 
-git clone https://github.com/ObolNetwork/charon-distributed-validator-node.git
-chmod o+w charon-distributed-validator-node
-cd charon-distributed-validator-node
-docker run --rm -v "$(pwd):/opt/charon" ghcr.io/obolnetwork/charon:v0.8.0 create enr
+DIR="charon-distributed-validator-node"
+CHARON_VER="v0.13.0"
+
+sudo apt update
+sudo apt install mc wget curl git htop net-tools unzip jq build-essential ncdu tmux -y
+bash <(curl -s https://raw.githubusercontent.com/f5nodes/root/main/install/docker.sh) &>/dev/null
+
+git clone https://github.com/ObolNetwork/$DIR.git
+chmod -R 707 $HOME/$DIR && cd $DIR
+
+sudo docker run --rm -v "$(pwd):/opt/charon" obolnetwork/charon:$CHARON_VER create enr
 
 echo -e "\n\e[93mYour \e[92menr\e[93m is above! \e[92mSave it!\e[0m"
 echo -e "See one more: \e[92mcat $HOME/charon-distributed-validator-node/.charon/charon-enr-private-key\e[0m"
