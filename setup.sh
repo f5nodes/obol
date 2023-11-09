3ef3e905fe599b9069d918f6c0e5fe5e046d1efc
Influenzo <52459025+vasylcode@users.noreply.github.com>
2023-11-09 12:24:09 -0700

    lido-charon-distributed-validator-node

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-DIR="charon-distributed-validator-node"
-CHARON_VER="v0.13.0"
+DIR="lido-charon-distributed-validator-node"
+CHARON_VER="v0.17.1"
 
 sudo apt update
 sudo apt install mc wget curl git htop net-tools jq build-essential ncdu tmux -y
 bash <(curl -s https://raw.githubusercontent.com/f5nodes/root/main/install/docker.sh) &>/dev/null
 
 git clone https://github.com/ObolNetwork/$DIR.git
-chmod -R 707 $HOME/$DIR && cd $DIR
+cd $DIR
 
-sudo docker run --rm -v "$(pwd):/opt/charon" obolnetwork/charon:$CHARON_VER create enr
+sudo docker run -u $(id -u):$(id -g) --rm -v "$(pwd):/opt/charon" obolnetwork/charon:$CHARON_VER create enr
 
 echo -e "\n\e[93mYour \e[92menr\e[93m is above! \e[92mSave it!\e[0m"
-echo -e "See one more: \e[92mcat $HOME/charon-distributed-validator-node/.charon/charon-enr-private-key\e[0m"
+echo -e "Please make sure to create a backup of the private key create at \e[92m.charon/charon-enr-private-key\e[0m"
